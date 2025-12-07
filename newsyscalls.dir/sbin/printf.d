5a5665c67f6fceeed65c98a32bacfa1a06b59327
Ryan Park <rpark@cs.utexas.edu>
2025-12-07 15:37:37 -0600

    deleted some extreneous files

--- newsyscalls.dir/sbin/printf.d
+++ newsyscalls.dir/sbin/printf.d
@@ -1,2 +1,2 @@
 printf.o: printf.c /usr/include/stdc-predef.h libc.h sys.h stdint.h \
- /u/gheith/public/cs439/lib/gcc/x86_64-pc-linux-gnu/14.1.0/include/stdarg.h
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h
